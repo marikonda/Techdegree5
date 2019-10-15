04aaf849b13a041254e10a1931577c4e05e6dd17
Mariko <53835688+marikonda@users.noreply.github.com>
2019-10-14 20:25:46 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
 $(document).ready(function(){
+  // take the information from the search input
   let images = document.getElementsByTagName('a');
+
+  // get the text that the user has typed
+  //use toLowerCase incase the user uses upper case
   $('.search_box').on('keyup',function(){
   let search = $('.searchInput').val().toLowerCase();
+
+  //use a loop to get a particular attribute
   for (var i=0; i<images.length; i++) {
     let searchVal= images[i].getAttribute('data-title');
+    // use of a conditional to look for a particular condition.
     if (searchVal.toLowerCase().indexOf(search)> -1) {
       images[i].style.display="";
     } else {
@@ -12,4 +19,3 @@ $(document).ready(function(){
    } 
   });
 });
-
